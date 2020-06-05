1d98a3428acbcb987301dc5fa93d4beae149fd14
Sarah Hart Landolt <sarah.hart.landolt@gmail.com>
2020-06-05 12:00:16 -0500

    initital commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Group2_HeistPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
